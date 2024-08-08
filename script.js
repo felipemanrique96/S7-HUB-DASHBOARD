29ea36292a8ad86ae054d5d474d69df13228b691
felipemanrique96 <felipe.manrique@settanta7.com>
2024-08-08 11:12:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         userInfo.textContent = 'Logged in';
     }
+
+    const workflowBox = document.getElementById('workflow-box');
+    const projectsDatabaseBox = document.getElementById('projects-database-box');
+
+    workflowBox.addEventListener('click', () => {
+        window.open('https://felipemanrique96.github.io/S7-HUB-Workflow/', '_blank');
+    });
+
+    projectsDatabaseBox.addEventListener('click', () => {
+        window.open('https://felipemanrique96.github.io/S7-HUB-Elenco-Progetti/', '_blank');
+    });
 });
